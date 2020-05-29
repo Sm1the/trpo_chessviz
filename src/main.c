1313ff5e16baf39a13136ec243515dc72c5e20b9
Sm1the <apxahreji123@gmail.com>
2020-05-30 03:40:09 +0700

    Fix

--- src/main.c
+++ src/main.c
@@ -1,17 +1,12 @@
 #include "board.h"
 #include "board_print_plain.h"
 #include "board_start.h"
-#include <cstdio>
 
 int main()
 {
-    printf("Welcome to the game \"Chess\".\nTo do at least, enter in the "
-           "console \"start position of the turn and end position of the "
-           "move\"\nBe sure to indicate the sequence through the dash.\n");
     char deck[8][8];
     board_start(deck);
     while (1) {
-        printf("\n");
         board_print_plain(deck);
         board(deck);
     }
